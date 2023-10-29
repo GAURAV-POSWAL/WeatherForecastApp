cf0429521f548de1cef46e4ca8192e431eba9afc
Gaurav Poswal <contact.mr.poswal@gmail.com>
2023-10-29 18:16:00 +0530

    - Update Dagger-Hilt dependencies - Import Retrofit & Gson to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,24 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     /*Dagger Hilt dependencies*/
-    implementation("com.google.dagger:hilt-android:2.46")
-    kapt("com.google.dagger:hilt-android-compiler:2.46")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    /*Retrofit dependencies*/
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    /*Google Gson dependency*/
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
